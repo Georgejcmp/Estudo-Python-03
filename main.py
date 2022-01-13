2d876361291d6a556d2856801fc289ea1dafd58f
Georgejcmp <george.moreno@yahoo.com.br>
2022-01-13 17:52:40 -0300

    Commit second

---
+++ main.py
@@ -0,0 +1,14 @@
+
+print('Qual o seu nome? :')
+
+nome = input()
+
+print('Que nome lindo você tem', nome)
+
+print(nome, 'qual a sua altura ?:')
+
+altura = float(input())
+
+print('Que foda', nome, 'com', altura, 'voce pode ser considerado um GIGANTE')
+
+
